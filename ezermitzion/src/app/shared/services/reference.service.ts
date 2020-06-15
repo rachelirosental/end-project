5d53465526faa9471f4b30e06a6f2541f9387621
rachelirosental <64493478+rachelirosental@users.noreply.github.com>
2020-06-15 19:24:26 +0300

    1

---
+++ ezermitzion/src/app/shared/services/reference.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Reference } from 'src/app/Data/Reference';
+@Injectable({
+    providedIn: 'root'
+  })
+  export class referenceservice{
+
+  }
+
+  
