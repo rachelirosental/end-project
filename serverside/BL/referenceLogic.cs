a3ad7aad5b347c417dd4961bdd9938119ba1b2ef
rachelirosental <64493478+rachelirosental@users.noreply.github.com>
2020-06-16 14:09:49 +0300

    1

--- serverside/BL/referenceLogic.cs
+++ serverside/BL/referenceLogic.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO;
+using DTO.NewFolder1;
 
 namespace BL
 {
-    class referenceLogic
+   public class referenceLogic
     {
+        public static DAL.EzerMitzionEntities db = new DAL.EzerMitzionEntities();
+       
+        public static bool AddReference(Reference reference)
+        {
+            try
+            {
+                db.Reference.Add(DTO.NewFolder1.Reference.GetReference(reference));
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+
+        public static int DeleteReference(int id)
+        {
+            var q1 = db.Reference.FirstOrDefault(w => w.RefId == id);
+            if (q1 == null)
+                return 0;
+            else
+            {
+                db.Reference.Remove(q1);
+                db.SaveChanges();
+                return 1;
+            }
+        }
+        public static Reference GetReference(int id)
+        {
+            var q1 = db.Reference.FirstOrDefault(w => w.RefId == id);
+            return Reference.GetReference(q1);
+
+        }
+        public static List<Reference> Allreference()
+        {
+            return Reference.GetListReferenceDTO(db.Reference.ToList());
+
+        }
+        public static void UpdateReference(Reference reference)
+        {
+            try
+            {
+                var ReferenceDb = db.Reference.FirstOrDefault(p => p.RefId == reference.RefId);
+                ReferenceDb.Date = reference.Date;
+                ReferenceDb.Description = reference.Description;
+                ReferenceDb.BornDate = reference.BornDate;
+                ReferenceDb.IsCare = reference.IsCare;
+                ReferenceDb.UserName = reference.UserName;
+                ReferenceDb.AttendantId = reference.AttendantId;
+                ReferenceDb.ProfId = reference.ProfId;
+                ReferenceDb.TypeRef = reference.TypeRef;
+                ReferenceDb.recommendation = reference.recommendation;
+                db.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+        }
+
     }
 }
--- serverside/serverside/Controllers/ReferenceController.cs
+++ serverside/serverside/Controllers/ReferenceController.cs
@@ -2,10 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using DTO.NewFolder1;
+using BL;
 
-namespace serverside.Controllers
+namespace API.Controllers
 {
-    public class ReferenceController
+    [RoutePrefix("api/User")]
+    public class ReferenceController:ApiController
     {
+        // GET: api/User
+        
+        [Route("getreference")]
+        public List<Reference> Get()
+        {
+            return BL.referenceLogic.Allreference();
+
+        }
+
+
+
+        // GET: api/User/5
+        [Route("getRefByid/{id}")]
+        public Reference Get(int id)
+        {
+            return BL.referenceLogic.GetReference(id);
+        }
+
+        [Route("create")]
+        // POST: api/User
+        public bool PostReference([FromBody]Reference reference)
+        {
+            return BL.referenceLogic.AddReference(reference);
+        }
+
+        [Route("update")]
+        // PUT: api/User/5
+        public void UpdateReference(Reference reference)
+        {
+            BL.referenceLogic.UpdateReference(reference);
+        }
+
+
+        [Route("delete/{id}")]
+        // DELETE: api/User/5
+        public static int Delete(int id)
+        {
+            return BL.referenceLogic.DeleteReference(id);
+        }
     }
 }
