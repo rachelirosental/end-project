bb87be96fce6ad12f6cfb8f0d2831a8396c69378
rachelirosental <64493478+rachelirosental@users.noreply.github.com>
2020-08-03 17:38:59 +0300

    1

---
+++ serverside/BL/ChartLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+public    class ChartLogic
+    {
+        public static DAL.EzerMitzionEntities db = new DAL.EzerMitzionEntities();
+       public Dictionary<string, int> counttype()
+        {
+            int count = 0;
+            Dictionary<string, int> typerefCount = new Dictionary<string, int>();
+            foreach(var type in db.TypeReference)
+            { count = db.Reference.Count(r => r.TypeRef == type.RefId);
+                typerefCount.Add(type.refname, count);
+
+
+            }
+            return typerefCount;
+        }
+
+    }
+}
