71ce4efe65627c12baef276f854fb1007358eee1
rachelirosental <64493478+rachelirosental@users.noreply.github.com>
2020-08-26 03:30:48 +0300

    26/08

--- ezermitzion/src/app/homepage/homepage.component.ts
+++ ezermitzion/src/app/homepage/homepage.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  public images:string[]=[];
+  public number:number=1;
+ 
 
-  constructor() { }
+  
+  constructor() {  setInterval(()=> { this.setActiveImage() }, 3000); }
 
   ngOnInit(): void {
+   
   }
-
-}
+  setActiveImage() {
+    this.number = this.number+1;
+    if(this.number==11)
+    this.number=1;
+}}
